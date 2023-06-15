6275fce01c342f0daee904233f69a60de4693d06
Mateusz <mateusz.wozniak.0211@gmail.com>
2023-06-15 13:59:39 +0200

    Scaled the board and added a chronometer

--- app/src/main/java/com/amnpa/puzzlecapture/PuzzleActivity.kt
+++ app/src/main/java/com/amnpa/puzzlecapture/PuzzleActivity.kt
@@ -1,17 +1,40 @@
 package com.amnpa.puzzlecapture
 
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.DisplayMetrics
 import android.util.Log
+import android.widget.Chronometer
 import android.widget.ImageView
-import com.google.android.material.card.MaterialCardView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_puzzle.*
 
+
 class PuzzleActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_puzzle)
+
+        val chronometer = findViewById<Chronometer>(R.id.chronometer)
+        chronometer.base=SystemClock.elapsedRealtime()
+        chronometer.start()
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val height = displayMetrics.heightPixels
+        val width = displayMetrics.widthPixels
+
+        val layoutParams = parentCoordinatorLayout.layoutParams
+        Log.v("dbg- h", layoutParams.height.toString())
+        Log.v("dbg- w", layoutParams.width.toString())
+        layoutParams.height = ((height-64) * 0.8).toInt()
+        layoutParams.width = (width * 0.8).toInt()
+        parentCoordinatorLayout.layoutParams = layoutParams
+        Log.v("dbg- h", layoutParams.height.toString())
+        Log.v("dbg- w", layoutParams.width.toString())
 
         val imgBitmap = this.intent.extras!!.getParcelable<Bitmap>("bitmap")
         val pt = PuzzleTransformer(this, imgBitmap!!)
@@ -19,10 +42,6 @@ class PuzzleActivity : AppCompatActivity() {
         tiles.sort()
 
         for (i in 0 until tiles.size){
-//            val card = MaterialCardView(this)
-//            card.isClickable = true
-//            card.isFocusable = true
-
             val imageView = ImageView(this)
             val usedBitmap = tiles[i].bitmap
             imageView.setImageBitmap(usedBitmap)
@@ -30,12 +49,8 @@ class PuzzleActivity : AppCompatActivity() {
             imageView.isFocusable = true
             parentCoordinatorLayout.addView(imageView)
             parentCoordinatorLayout.addDraggableChild(imageView)
-
-//            card.addView(imageView)
-//
-//            parentCoordinatorLayout.addView(card)
-//            parentCoordinatorLayout.addDraggableChild(card)
         }
+
 
     }
 }
